8230e7f1cc5d040cc1bfee185fc69abdd0b6960f
Dominik Schmidt <schmidom@student.ethz.ch>
2017-09-25 16:45:06 +0200

    Add additional mounts (dev/pts var/tmp/portage)
    the former is necessary, if we compile glibc[-suid], the latter
    for compiling large stuff like gcc, which would otherwise fill our
    rather small image

--- openstack_imagetest/generate_openstack.sh
+++ openstack_imagetest/generate_openstack.sh
@@ -7,7 +7,7 @@ function debug(){
 function cleanup(){
 	popd
 	debug "Unmounting Stuff"
-	umount gentoo/{dev,proc,sys,usr/portage}
+	umount gentoo/{dev/pts,dev,var/tmp/portage,proc,sys,usr/portage}
 	
 	debug "unmounting image"
 	umount gentoo/
@@ -88,6 +88,11 @@ debug "mount proc/"
 mount -t proc none proc
 debug "mount sys/"
 mount -t sysfs none sys
+debug "mount devpts with gid=5 for glibc[-suid] at /dev/pts/"
+mount -t devpts -o gid=5 none dev/pts/
+debug "mount tmpfd on /var/tmp/portage"
+mkdir -p var/tmp/portage
+mount -t tmpfs -o size=4G none var/tmp/portage
 popd
 
 
