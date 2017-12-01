3498becd304af3cc4554a150de09bb7d73be9fa1
Dominik Schmidt <das1993@hotmail.com>
2017-12-01 16:01:07 +0100

    Euler Script: nsswitch doesn't have to be copied, since the host /etc/nsswitch is used anyways
    /etc/nsswitch.conf is hardcoded into glibc. Therefore we can now run the script
    prior to running the bootstrap.

--- Euler/euler.sh
+++ Euler/euler.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-EPREFIX=${EPREFIX:-${SCRATCH}/gentoo}
+EPREFIX=${1:-${SCRATCH}/gentoo}
 
 cat >>~/.bashrc <<-EOF
 export -n LD_LIBRARY_PATH
 unset LD_LIBRARY_PATH
 EOF
 
-pushd ${EPREFIX}/lib/
-ln -s /usr/lib64/libnss_sss.so* .
-cd ../etc/
-mv nsswitch.conf nsswitch.conf.bk
-ln -s /etc/nsswitch.conf .
-popd
+for b in 32 64
+do
+	dir="${EPREFIX}/lib${b}/"
+	mkdir -p "${dir}"
+	ln -s -t "${dir}" /usr/lib${b}/libnss_sss.so* 
+done
