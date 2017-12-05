26fa6cc5271958df41ef265737219908a9948f1e
Dominik Schmidt <schmidom@student.ethz.ch>
2017-12-05 10:22:08 +0100

    Various changes in scripts

---
+++ BuildServer/scripts/openstack_image/default/10-prepare_image.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+debug "Allocating ${OPENSTACK_IMAGE_SIZE}G for $OPENSTACK_IMAGE"
+
+fallocate -l "${OPENSTACK_IMAGE_SIZE}G" "${OPENSTACK_IMAGE}"
+on_error "rm ${OPENSTACK_IMAGE}"
+
+debug "Setting up loopback"
+OPENSTACK_IMG_LODEV="$(losetup --show -f "${OPENSTACK_IMAGE}")"
+debug "Got loopback OPENSTACK_IMG_LODEV"
+on_exit "losetup -d ${OPENSTACK_IMG_LODEV}"
+
+debug "Setting up partition table"
+sfdisk $OPENSTACK_IMG_LODEV <<-EOF
+label: dos
+- - L *
+EOF
+
+debug "Copying syslinux MBR"
+dd if=/usr/share/syslinux/mbr.bin "of=$OPENSTACK_IMG_LODEV" conv=notrunc bs=440 count=1
+partx -u "$OPENSTACK_IMG_LODEV"
+
+debug "Formatting the disk with ext4"
+
+#Syslinux can't handle 64bit, so we disable it.
+#This has the effect, that our root partition can't grow larger than 2TB
+mkfs.ext4 -O ^64bit -L NeuroGentoo "${OPENSTACK_IMG_LODEV}p1"
+
+OPENSTACK_IMG_UUID="$(dumpe2fs ${OPENSTACK_IMG_LODEV}p1 | sed -n 's/.*UUID:[[:space:]]\+\([a-z0-9\-]\+\).*/\1/p')"
