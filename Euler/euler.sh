757dc0ec360383be8fa745cd15a38fb70a0deb1b
Dominik Schmidt <das1993@hotmail.com>
2017-12-09 17:46:07 +0100

    Combine the two euler scripts into one

--- Euler/euler.sh
+++ Euler/euler.sh
@@ -2,6 +2,23 @@
 
 set -e
 
+function prepare_euler(){
+	cat >>~/.bashrc <<-EOF
+export -n LD_LIBRARY_PATH
+unset LD_LIBRARY_PATH
+EOF
+
+	for b in 32 64
+	do
+		dir="${EPREFIX}/lib${b}/"
+		mkdir -p "${dir}"
+		ln -s -t "${dir}" /usr/lib${b}/libnss_sss.so* 
+	done
+
+	mkdir "${EPREFIX}/etc"
+	cp -L /etc/nsswitch.conf "${EPREFIX}/etc/"
+}
+
 if [ "$1" == "-h" -o "$1" == "--help" ]
 then
 	echo "Usage: $0 [/path/to/prefix]"
@@ -10,7 +27,7 @@ fi
 
 EPREFIX="${1:-${SCRATCH}/gentoo}"
 
-"${0%/*}/prepare_euler.sh" "${EPREFIX}"
+prepare_euler
 cd "$SCRATCH"
 wget "https://dev.gentoo.org/~heroxbd/bootstrap-rap.sh"
 chmod a+x bootstrap-rap.sh
