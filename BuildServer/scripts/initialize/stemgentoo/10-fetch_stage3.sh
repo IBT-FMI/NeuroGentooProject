b348676d60a5344a87e95ea118cc01513813789c
Horea Christian <horea.christ@yandex.com>
2018-01-09 14:05:07 +0100

    Adapted to upstream format migration

--- BuildServer/scripts/initialize/stemgentoo/10-fetch_stage3.sh
+++ BuildServer/scripts/initialize/stemgentoo/10-fetch_stage3.sh
@@ -4,10 +4,10 @@ set -e
 GENTOO_MIRROR="http://distfiles.gentoo.org/releases/amd64/autobuilds"
 
 debug "Fetching the newest gentoo stage3 url"
-file="$(curl "${GENTOO_MIRROR}/latest-stage3-amd64.txt" | sed -n 's/\(\+*\.tar\.bz2\).*/\1/p')"
+file="$(curl "${GENTOO_MIRROR}/latest-stage3-amd64.txt" | sed -n 's/\(\+*\.tar\.xz\).*/\1/p')"
 filename="${file##*/}"
 cachefile="${CACHE}/$filename"
 debug "it is ${file}, downloading ${GENTOO_MIRROR}/${file} -> ${filename}:"
 [ -f "${cachefile}" ] || curl -o "${cachefile}" "${GENTOO_MIRROR}/${file}"
-rm "${CACHE}/stage3_latest.tar.bz2" -f
-ln -s "${cachefile}" "${CACHE}/stage3_latest.tar.bz2"
+rm "${CACHE}/stage3_latest.tar.xz" -f
+ln -s "${cachefile}" "${CACHE}/stage3_latest.tar.xz"
--- BuildServer/scripts/initialize/stemgentoo/11-unpack_stage3.sh
+++ BuildServer/scripts/initialize/stemgentoo/11-unpack_stage3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tar xvjf "${CACHE}"/stage3_latest.tar.bz2 -C "${ROOT}"
+tar xvJf "${CACHE}"/stage3_latest.tar.xz -C "${ROOT}"
