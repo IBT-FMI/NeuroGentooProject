b25e8ae4d39997c0617f73f028bf57e3958e8fc4
Horea Christian <horea.christ@yandex.com>
2018-01-23 12:48:00 +0100

    Adapting to system-wide install

--- BuildServer/example_hooks/cronjob.sh
+++ BuildServer/example_hooks/cronjob.sh
@@ -6,9 +6,9 @@ ROOT_DIR="$(dirname "$(realpath "$0")")/../"
 ROOT_DIR="${1:-$ROOT_DIR}"
 
 pushd "${ROOT_DIR}"
-./exec.sh stemgentoo update
+buildserver stemgentoo update
 for file in roots/*/.gentoo/
 do
-	./exec.sh "$file" update
+	buildserver "$file" update
 done
 popd
