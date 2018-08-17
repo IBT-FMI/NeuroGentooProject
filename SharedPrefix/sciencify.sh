176380fb71bfb2752d104875f64af508605b9bd9
Horea Christian <horea.christ@yandex.com>
2018-08-17 21:11:52 +0200

    Addressing new upstream CMake issue
    https://github.com/gentoo/sci/issues/878

--- SharedPrefix/sciencify.sh
+++ SharedPrefix/sciencify.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-EPREFIX=${1:-$HOME/gentoo/}
+if $1 == "eprefix"; then
+	EPREFIX=${1:-$HOME/gentoo/}
+else
+	EPREFIX=""
+fi
 
 mkdir ${EPREFIX}/etc/portage/repos.conf
 
@@ -21,20 +25,4 @@ priority = 7777
 EOF
 emaint sync --repo science
 eix-update
-
-#Link to the workaroud we reproduce in this section : https://wiki.gentoo.org/wiki/User_talk:Houseofsuns#Migration_to_science_overlay_from_main_tree
-#Efforts to more permanently address the issue: https://github.com/gentoo/sci/issues/805
-echo ""
-echo "Setting Up Eselect for Gentoo Science:"
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-cp ".gentoo/files/sci-lapack" "${EPREFIX}/etc/portage/package.mask/"
-emerge --oneshot --verbose app-admin/eselect::science >> /dev/null
-FEATURES="-preserve-libs":$FEATURES emerge --oneshot --verbose sci-libs/blas-reference::science
-eselect blas set reference
-FEATURES="-preserve-libs":$FEATURES emerge --oneshot --verbose sci-libs/cblas-reference::science
-eselect cblas set reference
-FEATURES="-preserve-libs":$FEATURES emerge --oneshot --verbose sci-libs/lapack-reference::science
-eselect lapack set reference
-FEATURES="-preserve-libs":$FEATURES emerge --oneshot --verbose --exclude sci-libs/blas-reference --exclude sci-libs/cblas-reference --exclude sci-libs/lapack-reference `eix --only-names --installed --in-overlay science`
-
-emerge -1qv @preserved-rebuild
+./lapack-sci.sh
